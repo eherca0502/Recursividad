59563b8cca4c46031ef1d8027bc5a7dd91ad1113
eherca0502 <ehc0502@outlook.com>
2023-08-31 21:58:20 -0600

    Agregar archivos de proyecto.

---
+++ Recursividad/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1,2,3,4,5};
+            MostrarArrayRecursivo(array, 0);
+            Console.ReadLine();
+        }
+        public static void MostrarArrayRecursivo(int[] array, int indice)
+        {
+            if(indice != array.Length)
+            {
+                Console.WriteLine(array[indice]);
+                MostrarArrayRecursivo(array, indice + 1);
+            }
+            
+          
+        }
+    }
+}
